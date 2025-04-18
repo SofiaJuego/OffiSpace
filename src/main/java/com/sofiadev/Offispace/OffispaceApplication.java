5581d2c8c323710693fc2e7360a20f3a0a98de37
SofiaJuego <sofiaguilar767@gmail.com>
2025-04-18 16:54:59 -0300

    initial commit: proyecto Offispace con estructura base.

---
+++ src/main/java/com/sofiadev/Offispace/OffispaceApplication.java
@@ -0,0 +1,13 @@
+package com.sofiadev.Offispace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffispaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffispaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sofiadev/Offispace/OffispaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sofiadev.Offispace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffispaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
