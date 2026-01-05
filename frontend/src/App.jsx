f6fadf31bd3c4db2297da81f3d5962d842afcc05
sa767 <sofia.aguilar.cv@gmail.com>
2026-01-04 22:24:28 -0300

    diseño de login y registro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,18 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../src/features/home/HomePage";
 import SpaceDetail from "../src/features/spacedetail/SpaceDetail"
+import SearchResults from "./features/search/SearchResults";
+import LoginPage from "./features/login-register/login/LoginPage"
 
 
 
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<HomePage />} /> */}
-      <Route path="/" element={<SpaceDetail />} />
+      <Route path="/" element={<LoginPage />} />
+      {/* <Route path="/" element={<SearchResults />} /> */}
+      {/* <Route path="/" element={<HomePage />} />  */}
+      {/* <Route path="/" element={<SpaceDetail />} /> */}
     </Routes>
   );
 }
